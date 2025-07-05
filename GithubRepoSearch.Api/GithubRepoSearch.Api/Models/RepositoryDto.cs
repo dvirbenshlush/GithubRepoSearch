0d816ea0cc5f5c2d4a828357526924da161363dc
dvirbenshlush <dvirbsh95@gmail.com>
2025-07-05 18:06:52 +0300

    add search github repo search feature

---
+++ GithubRepoSearch.Api/GithubRepoSearch.Api/Models/RepositoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GithubRepoSearch.Api.Models;
+
+public class RepositoryDto
+{
+    public string Name { get; set; }
+    public string OwnerAvatarUrl { get; set; }
+    public string HtmlUrl { get; set; }
+}
---
+++ GithubRepoSearch.Api/GithubRepoSearch.Api/Services/IGitHubService.cs
@@ -0,0 +1,8 @@
+﻿using GithubRepoSearch.Api.Models;
+
+namespace GithubRepoSearch.Api.Services;
+
+public interface IGitHubService
+{
+    Task<List<RepositoryDto>> SearchRepositoriesAsync(string keyword);
+}
