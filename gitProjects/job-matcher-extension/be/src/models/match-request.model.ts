01286c8f59875615a2f63c93a5f0382f884c4983
Dvir Ben Shlush <58987189+dvirbenshlush@users.noreply.github.com>
2025-07-05 16:45:44 +0300

    Delete gitProjects/job-matcher-extension directory

--- gitProjects/job-matcher-extension/be/src/models/match-request.model.ts
+++
@@ -1,6 +0,0 @@
-export interface MatchRequestBody {
-    resume: string;
-    preferences: string;
-    jobDesc: string;
-  }
-  
--- gitProjects/job-matcher-extension/be/src/routes/match.routes.ts
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getMatchScore } = require('../controllers/match.controller');
-const router = express.Router();
-
-// POST /match
-router.post('/', getMatchScore);
-
-export default router; // ⚠️ זה חשוב!
