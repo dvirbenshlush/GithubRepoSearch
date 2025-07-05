ad5ec01f2ece4db9a8cadbb5cc8ca2d8c3599894
Dvir Ben Shlush <58987189+dvirbenshlush@users.noreply.github.com>
2025-07-05 16:46:00 +0300

    Delete BankTransactionsClient directory

--- BankTransactionsClient/src/app/models/transaction.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Transaction {
-    isEditing?: boolean;
-    accountNumber: string;
-    amount: number;
-    externalTransactionId: string;
-    status?: string;
-    identityNumber: string;
-    transactionDate: Date;
-}
--- BankTransactionsClient/src/app/state/bank/transaction.selectors.ts
+++
@@ -1,4 +0,0 @@
-import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { Transaction } from '../../models/transaction.model';
-
-export const selectTransactions = createFeatureSelector<ReadonlyArray<Transaction>>('transactions');
