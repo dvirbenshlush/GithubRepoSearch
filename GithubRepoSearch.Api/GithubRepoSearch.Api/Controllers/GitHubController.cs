534f34a2dbad4bff1bd27153f455a6a5ac686440
dvirbenshlush <dvirbsh95@gmail.com>
2025-07-06 21:57:47 +0300

    create bookmark server side

--- GithubRepoSearch.Api/GithubRepoSearch.Api/Controllers/GitHubController.cs
+++ GithubRepoSearch.Api/GithubRepoSearch.Api/Controllers/GitHubController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using GithubRepoSearch.Api.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GithubRepoSearch.Api.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class GitHubController : ControllerBase
